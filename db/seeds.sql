151f37c29ee7576ff19771c479c023c05c3e70bb
courtney manery <courtneymmanery@gmail.com>
2019-05-11 01:40:04 -0400

    adding styling

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,4 @@ INSERT INTO burgers (burgers_name, devoured) VALUES ('Hamburger', false);
 INSERT INTO burgers (burgers_name, devoured) VALUES ('Veggie Burger', false);
 INSERT INTO burgers (burgers_name, devoured) VALUES ('Cheeseburger', false);
 INSERT INTO burgers (burgers_name, devoured) VALUES ('Bacon Burger', true);
-INSERT INTO burgers (burgers_name, devoured) VALUES ('Cheese Burger', true);
+INSERT INTO burgers (burgers_name, devoured) VALUES ('DemBurger', true);
