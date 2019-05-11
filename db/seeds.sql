9e9ec28392e2afd3df4ed3fe3fb027d098391ebc
courtney manery <courtneymmanery@gmail.com>
2019-05-10 20:48:41 -0400

    First Commit

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burgers_name, devoured) VALUES ('Hamburger', false);
+INSERT INTO burgers (burgers_name, devoured) VALUES ('Veggie Burger', false);
+INSERT INTO burgers (burgers_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burgers_name, devoured) VALUES ('Bacon Burger', true);
+INSERT INTO burgers (burgers_name, devoured) VALUES ('Cheese Burger', true);
