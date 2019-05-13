2b735ae9bb974c5a4d6af74a4e85cfff6055cf96
courtney manery <courtneymmanery@gmail.com>
2019-05-13 10:01:10 -0400

    fixing controllers

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -14,8 +14,8 @@ router.get("/", function (req, res) {
 
     router.post("/api/burgers", function (req, res) {
         burger.insertOne(
-            ["burger_name", "devoured"],
-            [req.body.burger_name, req.body.devoured],
+            ["burgers_name", "devoured"],
+            [req.body.burgers_name, req.body.devoured],
             function (result) {
                 // Send back the ID of new burger
                 res.json({ id: result.insertId });
