e7437401a8bce3bc7884b63c8dba3d314739eb3b
courtney manery <courtneymmanery@gmail.com>
2019-05-13 15:16:02 -0400

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: "localhost",
