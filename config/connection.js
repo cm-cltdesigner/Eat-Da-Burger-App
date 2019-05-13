6a5dedf1d5fae4733c5e747cae8e49d90585cdf4
courtney manery <courtneymmanery@gmail.com>
2019-05-13 15:05:09 -0400

    connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 var mysql = require("mysql");
-connection = mysql.createConnection({
-    host: "localhost",
-    port: "8889",
-    user: "root",
-    password: "root",
-    database: "burgers_db"
-});
-connection.connect(function(err) {
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: "8889",
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
+connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
         return;
--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,7 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
-
 CREATE TABLE burgers(
     id INT NOT NULL AUTO_INCREMENT,
     burgers_name VARCHAR(50) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
